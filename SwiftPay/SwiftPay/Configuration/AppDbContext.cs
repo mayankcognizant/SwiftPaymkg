d6d4421684f15821e3d36a9589edd30cc9167361
Shaikh <2479413@cognizant.com>
2026-03-09 14:35:51 +0530

    Created the  base folder

---
+++ SwiftPay/SwiftPay/Configuration/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SwiftPay.Configuration
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+
+        
+    }
+}
